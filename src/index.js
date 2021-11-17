fbf532f580b05b012ec16b8cc7445cb59f458a0a
Jeff Terrell <jeffaterrell@gmail.com>
2021-11-17 14:01:54 -0500

    add logic for bike api
    Co-Authored-by: Marisa Edgar <marisaedgar1212@gmail.com>
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,55 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import BikeRegService from "./bikereg.js";
 
+// function clearFields() {
+//   $('#location').val("");
+// }
 
 $(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let promise = BikeRegService.getBikeReg(city);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      for (let i = 0; i < body.bikes.length; i++) {
+        $('.brandName').append(`The manufacturer of the bike is ${body.bikes[i].manufacturer_name} ${body.bikes[i].frame_model} ${body.bikes[1].serial}`);
+        $('.image').append('<img src="' + body.bikes[i].large_img + '">');
+      }  
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
   
+    });
+  });
 });
+
+
+
+
+
+//example
+
+// function clearFields() {
+//   $('#location').val("");
+//   $('.showErrors').text("");
+//   $('.showHumidity').text("");
+//   $('.showTemp').text("");
+// }
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     clearFields();
+//     let promise = WeatherService.getWeather(city);
+//     promise.then(function(response) {
+//       const body = JSON.parse(response);
+//       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+//       $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+//     }, function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error}`);
+//     });
+//   });
+// });
